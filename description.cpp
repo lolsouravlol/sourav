ed4982e6d518de4687913fad1278a6f37282be87
lolsouravlol <souravmuduly7@gmail.com>
2025-03-29 16:24:39 +0530

    MY description

---
+++ description.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "I am Sourav Muduly.\nI am from the department of Mechanical Engg.\nI am from Rourkela,Odisha." << std::endl;
+    return 0;
+}
